672c130a4ff1629967ba873dce143e11ecaadef0
Christophe <chris.cw@outlook.fr>
2025-08-29 03:09:41 +0200

    mise à jour de la page d'accueil desktop et mobile

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
   <header class="desktop-header">
   <a href="index.php" class="logo">Parkoo</a>
   <nav class="desktop-nav">
-    <a href="#">Accueil</a>
     <a href="#">Trouver une place</a>
-    <a href="#">Connexion</a>
+    <a href="#">Partager une place</a>
+    <a href="#">Se connecter</a>
   </nav>
 </header>
 
@@ -22,7 +22,7 @@
   <h1 class="logo">Parkoo</h1>
   
   <section class="search_section">
-    <h2 class="subtitle">Le Parking<br />Partagé</h2>
+    <h2 class="subtitle">Le Parking Partagé</h2>
 
     <div class="hero__buttons">
       <a href="#" class="btn btn--light">Trouver une place</a>
@@ -30,16 +30,27 @@
     </div>
   </section>
 </main>
-
+  <section class="about">
+    <h2 class="subtitle">Parkoo, le parking simplifié entre particuliers</h2>
+    <p>
+      Parkoo est une plateforme qui permet de trouver et réserver facilement 
+      une place de parking disponible chez des particuliers, partout en France.
+      Que vous soyez conducteur à la recherche d'un stationnement ou propriétaire 
+      d'une place libre, Parkoo vous connecte pour vous offrir confort et simplicité 
+      au quotidien.
+    </p>
+  </section>
   <nav class="bottom-nav">
     <a href="#">Accueil</a>
     <a href="#">Trouver une place</a>
     <a href="#">Connexion</a>
   </nav>
 
-  <footer class="desktop-footer">
-  <p>&copy; 2025 Parkoo. Tous droits réservés.</p>
-  <a href="#" class="legal">Mentions légales</a>
+  <footer class="footer">
+    <div class = "footer-container">
+      <p>&copy; 2025 Parkoo. Tous droits réservés.</p>
+      <a href="#" class="legal">Mentions légales</a>
+    </div>
 </footer>
 
 </body>
