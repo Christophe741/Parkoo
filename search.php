8f4cb9e9df50cfa229e7ecc72dae6c7e4ba743f6
Christophe <chris.cw@outlook.fr>
2025-08-31 01:11:50 +0200

    ajout search.php

--- search.php
+++ search.php
@@ -20,26 +20,16 @@
 
 <main class="hero">
   <h1 class="logo">Parkoo</h1>
-  
-  <section class="search_section">
-    <h2 class="subtitle">Le Parking Partagé</h2>
-
-    <div class="hero__buttons">
-      <a href="search.php" class="btn btn--light">Trouver une place</a>
-      <a href="#" class="btn btn--dark">Partager une place</a>
-    </div>
-  </section>
+    <section class="results">
+     <h2 class="subtitle">Résultats de recherche</h2>
+     <div id="results">
+       <template id="parking-card">
+         <p>test</p>
+       </template>
+     </div>
+   </section>
 </main>
-  <section class="about">
-    <h2 class="subtitle">Parkoo, le parking simplifié entre particuliers</h2>
-    <p>
-      Parkoo est une plateforme qui permet de trouver et réserver facilement 
-      une place de parking disponible chez des particuliers, partout en France.
-      Que vous soyez conducteur à la recherche d'un stationnement ou propriétaire 
-      d'une place libre, Parkoo vous connecte pour vous offrir confort et simplicité 
-      au quotidien.
-    </p>
-  </section>
+
   <nav class="bottom-nav">
     <a href="#">Accueil</a>
     <a href="#">Trouver une place</a>
